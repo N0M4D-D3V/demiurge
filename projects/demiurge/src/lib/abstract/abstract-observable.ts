a4b1d139aaa235bd27be66746104da860946443e
N0M4D <victor.develops@gmail.com>
2024-01-18 19:35:36 +0100

    <> DemiObservable now has two implementations: list and just unike<>

--- projects/demiurge/src/lib/abstract/abstract-observable.ts
+++ projects/demiurge/src/lib/abstract/abstract-observable.ts
@@ -1,10 +1,25 @@
 import { BehaviorSubject, Observable } from 'rxjs';
 
-export abstract class DemiObservable<T> {
-  protected bs: BehaviorSubject<T[]> = new BehaviorSubject<T[]>([]);
-  protected $obs: Observable<T[]> = this.bs.asObservable();
+export interface DemiObservableInterface<T> {
+  bs: BehaviorSubject<T>;
+  $obs: Observable<T>;
+  value?: T;
 
-  public value: T[] = [];
+  emit(): void;
+  update(value: T): void;
+  $get(): Observable<T>;
+  clear(): void;
+}
+
+export abstract class DemiObservableList<T extends Array<T>>
+  implements DemiObservableInterface<T | undefined>
+{
+  public bs: BehaviorSubject<T | undefined> = new BehaviorSubject<
+    T | undefined
+  >(undefined);
+  public $obs: Observable<T | undefined> = this.bs.asObservable();
+
+  public value?: T;
 
   /**
    * Emits the stored value if it exists
@@ -17,7 +32,7 @@ export abstract class DemiObservable<T> {
    * Updates the stored value and emits the updated value
    * @param value
    */
-  public updateObservable(value: T[]): void {
+  public update(value: T): void {
     this.value = value;
     this.bs.next(value);
   }
@@ -26,15 +41,58 @@ export abstract class DemiObservable<T> {
    * Returns the observable
    * @returns
    */
-  public getObservable(): Observable<T[]> {
+  public $get(): Observable<T | undefined> {
     return this.$obs;
   }
 
   /**
    * Clears the stored value and emits an empty array to clear the observable
    */
-  public clearObservable(): void {
-    this.value = [];
-    this.bs.next([]);
+  public clear(): void {
+    this.value = undefined;
+    this.bs.next(undefined);
+  }
+}
+
+export abstract class DemiObservable<T>
+  implements DemiObservableInterface<T | undefined>
+{
+  public bs: BehaviorSubject<T | undefined> = new BehaviorSubject<
+    T | undefined
+  >(undefined);
+  public $obs: Observable<T | undefined> = this.bs.asObservable();
+
+  public value?: T;
+
+  /**
+   * Emits the stored value if it exists
+   */
+  public emit(): void {
+    this.bs.next(this.value);
+  }
+
+  /**
+   * Updates the stored value and emits the updated value
+   * @param value
+   */
+  public update(value: T): void {
+    this.value = value;
+    this.bs.next(value);
+  }
+
+  /**
+   * Returns the observable
+   * @returns
+   */
+  public $get(): Observable<T | undefined> {
+    return this.$obs;
+  }
+
+  /**
+   * Clears the stored value and emits an empty array to clear the observable
+   */
+  public clear(): void {
+    this.value = undefined;
+    this.bs.next(undefined);
   }
 }
